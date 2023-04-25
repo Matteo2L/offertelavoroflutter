b7119dac3c274e7cb3ab1e80aafd5182773c07e9
Matteo Lazzaro <matteo.lazzaro@hidew.it>
2023-04-25 22:50:24 +0200

    Completato i services network per le offerte di lavoro

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:offertelavoroflutter/pages/main_page.dart';
+import 'package:offertelavoroflutter/repositories/mappers/job_mapper.dart';
+import 'package:offertelavoroflutter/services/network/jobs_service.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'title',
-        theme: ThemeData(),
-        home: const MainPage(),
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          Provider<JobMapper>(
+            create: (_) => JobMapper(),
+          )
+        ],
+        child: MultiProvider(
+          providers: [
+            Provider<JobsService>(
+              create: (_) => JobsService(),
+            ),
+          ],
+          child: MaterialApp(
+            title: 'title',
+            theme: ThemeData(),
+            home: const MainPage(),
+          ),
+        ),
       );
 }
--- lib/services/network/jobs_service.dart
+++ lib/services/network/jobs_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:offertelavoroflutter/errors/network_error.dart';
+import 'package:offertelavoroflutter/services/network/dto/job_announcements_response.dart';
 import 'package:offertelavoroflutter/services/network/dto/job_dto.dart';
 
 class JobsService {
@@ -9,12 +10,11 @@ class JobsService {
   static const baseUrl =
       'https://api.notion.com/v1/databases/283d2760f81548f0a7baca4b3e58d7d8/query';
 
-  Future<List<JobDTO>> jobs(String date) async {
+  Future<List<JobDTO>> jobs() async {
     final response = await http.post(
-      Uri.https(baseUrl, auth),
+      Uri.https(baseUrl, auth, {}),
       headers: <String, String>{
-        HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
-        'Notion-Version': 'bb6682d2a6ca79ddd9e56a2d05bd0bad',
+        HttpHeaders.contentTypeHeader: 'Notion-Version: 2021-05-11',
       },
     );
 
@@ -26,6 +26,26 @@ class JobsService {
     }
 
     final decodedResponse = jsonDecode(response.body) as Map<String, dynamic>;
-    return JobResponse.fromJson(decodedResponse).response;
+    return JobAnnouncementsResponse.fromJson(decodedResponse).results;
+  }
+
+  // Ritorna il json per intero
+  Future<JobAnnouncementsResponse> jobAnnouncement() async {
+    final response = await http.post(
+      Uri.https(baseUrl, auth, {}),
+      headers: <String, String>{
+        HttpHeaders.contentTypeHeader: 'Notion-Version: 2021-05-11',
+      },
+    );
+
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      throw NetworkError(
+        response.statusCode,
+        response.reasonPhrase,
+      );
+    }
+
+    final decodedResponse = jsonDecode(response.body) as Map<String, dynamic>;
+    return JobAnnouncementsResponse.fromJson(decodedResponse);
   }
 }
