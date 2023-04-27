16382eeaf350943f87df5bc0eea5234895df2cca
Matteo Lazzaro <matteo.lazzaro@hidew.it>
2023-04-27 12:34:47 +0200

    Sistemato bug su job service

--- lib/services/network/jobs_service.dart
+++ lib/services/network/jobs_service.dart
@@ -6,15 +6,17 @@ import 'package:offertelavoroflutter/services/network/dto/job_announcements_resp
 import 'package:offertelavoroflutter/services/network/dto/job_dto.dart';
 
 class JobsService {
-  static const auth = 'secret_Azc2DHy4JY0Ved0cD0ObrEFJqIaUqy96CboXgJZp8bZ';
-  static const baseUrl =
-      'https://api.notion.com/v1/databases/283d2760f81548f0a7baca4b3e58d7d8/query';
+  static String auth =
+      'Bearer secret_Azc2DHy4JY0Ved0cD0ObrEFJqIaUqy96CboXgJZp8bZ';
+  static String baseUrl = 'api.notion.com';
+  static String path = '/v1/databases/283d2760f81548f0a7baca4b3e58d7d8/query/';
 
   Future<List<JobDTO>> jobs() async {
     final response = await http.post(
-      Uri.https(baseUrl, auth, {}),
+      Uri.https(baseUrl, path),
       headers: <String, String>{
-        HttpHeaders.contentTypeHeader: 'Notion-Version: 2021-05-11',
+        HttpHeaders.authorizationHeader: auth,
+        'Notion-Version': '2022-06-28',
       },
     );
 
