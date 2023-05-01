7faad1c6e1d50742302096e5e7cf14e0366a0dc0
Matteo Lazzaro <matteo.lazzaro@hidew.it>
2023-05-01 10:56:50 +0200

    Utilizzato pine

--- lib/app.dart
+++ lib/app.dart
@@ -5,51 +5,48 @@ import 'package:offertelavoroflutter/pages/main_page.dart';
 import 'package:offertelavoroflutter/repositories/jobs_repository.dart';
 import 'package:offertelavoroflutter/repositories/mappers/job_mapper.dart';
 import 'package:offertelavoroflutter/services/network/jobs_service.dart';
+import 'package:pine/di/dependency_injector_helper.dart';
 import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => MultiProvider(
-        providers: [
+  Widget build(BuildContext context) => DependencyInjectorHelper(
+        mappers: [
           Provider<JobMapper>(
             create: (_) => JobMapper(),
           )
         ],
-        child: MultiProvider(
-          providers: [
-            Provider<JobsService>(
-              create: (_) => JobsService(),
+        providers: [
+          Provider<JobsService>(
+            create: (_) => JobsService(),
+          ),
+        ],
+        repositories: [
+          RepositoryProvider<JobsRepository>(
+            create: (context) => JobsRepository(
+              jobsService: context.read<JobsService>(),
+              jobMapper: context.read<JobMapper>(),
             ),
-          ],
-          child: MultiRepositoryProvider(
-            providers: [
-              RepositoryProvider<JobsRepository>(
-                create: (context) => JobsRepository(
-                  jobsService: context.read<JobsService>(),
-                  jobMapper: context.read<JobMapper>(),
-                ),
-              )
-            ],
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Jobs',
-              theme: ThemeData(
-                primarySwatch: MaterialColor(0xFF027DFD, color),
-                appBarTheme: const AppBarTheme(
-                  titleTextStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  backgroundColor: Colors.indigo,
-                  elevation: 5,
-                ),
+          )
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Jobs',
+          theme: ThemeData(
+            primarySwatch: MaterialColor(0xFF027DFD, color),
+            appBarTheme: const AppBarTheme(
+              titleTextStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              home: const MainPage(),
+              backgroundColor: Colors.indigo,
+              elevation: 5,
             ),
           ),
+          home: const MainPage(),
         ),
       );
 }
